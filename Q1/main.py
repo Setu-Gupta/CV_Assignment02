9c12be4ec43198a8b04c4e33b3a0a8da228ea333
Setu Gupta <setu.gupta.2020@gmail.com>
2023-04-01 22:42:13 +0530

    Added code for Q1.4
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   error.png
    	modified:   main.py
    	modified:   ../README

--- Q1/main.py
+++ Q1/main.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import glob
 import csv
+from matplotlib import pyplot as plt
 
 # Specify the pattern
 CHECKERBOARD = (9, 7) # The pattern has 9 corners per column and 7 corners per row
@@ -53,7 +54,7 @@ for fname in image_paths:
     cv2.imwrite(corner_path, img)
 
 # Perform camera calibration
-reproj_error, int_mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+error, int_mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
 # Extract the intrinsic parameters out of the intrinsic matrix
 fx = int_mtx[0][0]
@@ -65,7 +66,7 @@ cy = int_mtx[1][2]
 print("Estimated fx and fy focal lengths:", fx, fy)
 print("Estimated skew (OpenCV does NOT estimate the skew. It assumes it to be zero):", skew)
 print("Estimated optical center coordinates:", cx, cy)
-print("Estimated re-projection error:", reproj_error)
+print("Estimated error:", error)
 print("============================================")
 
 print("The extrinsic parameters for all the images:")
@@ -91,7 +92,7 @@ with open("extrinsics.csv", 'w', newline='') as csvfile:
         writer.writerow(row)
 print("============================================")
 
-print("Estimated Distortion coefficients:", dist)
+print("Estimated Distortion coefficients:", dist.reshape(-1))
 
 for img, fname in zip(images, images_with_corners):
     h, w = img.shape[:2]
@@ -105,4 +106,32 @@ for img, fname in zip(images, images_with_corners):
     dst = dst[y:y+h, x:x+w]
     output_image_name = fname.replace("images", "undistorted")
     cv2.imwrite(output_image_name, dst)
+print("============================================")
+
+reproj_errors = []  # An array to hold the reprojection error values
+image_names = []    # An array to hold the names of the images for which the reprojection error is computed
+reproj_corners = [] # An array to hold the reprojected image coordinates
+
+# Compute the errors
+for i in range(len(objpoints)):
+    projected_points, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], int_mtx, dist)
+    error = cv2.norm(imgpoints[i], projected_points, cv2.NORM_L2) / len(projected_points)
+    reproj_corners.append(projected_points)
+    reproj_errors.append(error)
+    image_names.append(images_with_corners[i].split('/')[-1].split('.')[0])
+reproj_errors = np.array(reproj_errors)
+
+# Find and print the mean and the standard deviation
+mean_error = np.mean(reproj_errors)
+std_dev_error = np.std(reproj_errors)
+print("Mean reprojection error:", mean_error)
+print("Standard deviation of reprojection error:", std_dev_error)
+
+# Create the bar graph for the errors
+plt.bar(image_names, reproj_errors)
+plt.xlabel("Image names")
+plt.ylabel("Re-projection Error")
+plt.title("Re-projection error for various images")
+plt.gcf().set_figwidth(10)
+plt.savefig("error.png")
 print("============================================")
